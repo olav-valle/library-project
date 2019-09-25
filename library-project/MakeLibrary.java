d73c1f69e23b04bfefa00fd593363c0e3ad19471
Olav Valle <olavval@stud.ntnu.no>
2019-09-25 21:26:12 +0200

    add work done

---
+++ library-project/MakeLibrary.java
@@ -0,0 +1,17 @@
+/**
+*	A class to generate a test library and fill it with 
+* 	a selection of book objects.
+*/
+
+public class MakeLibrary
+{
+	public MakeLibrary()
+	{
+	addBook("The Colour of Magic", "Terry Pratchett", "Corgi", 1985, 285, "9780552124751");
+        addBook("A first course in machine learning (Second edition)", "Simon Rogers, Mark Girolami", "CRC Press", 2017, 397, "9781498738484");
+        addBook("The Shadow of the Torturer", "Gene Wolfe", "Tom Doherty Associates, Inc.", 1982, 262, "9780671540661");
+        addBook("Molecular Gastronomy: Exploring the science of Flavor", "Hervé This", "Columbia University Press", 2006, 377, "9780231133128");
+        addBook("Les Halles Cookbook", "Anthony Bourdain", "Bloomsbury", 2004, 304, "9780747580126");
+        addBook("Larousse Gastronomique", "Prosper Montagné", "Éditions Larousse", 1938, 1087, "9780600620426");
+	}
+}
